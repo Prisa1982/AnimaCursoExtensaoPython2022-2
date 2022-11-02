b50e03eae1245b77d3931baab84f4a049c88f7a0
prisa1982 <116230317+Prisa1982@users.noreply.github.com>
2022-11-02 16:54:52 +0000

    Ensinei comando de entrada input() e falamos novamente sobre a diferença de string para int...

---
+++ aula-1-2022-10-19.py
@@ -0,0 +1,29 @@
+# Meu primeiro projeto Python!!!
+#
+# print()comando de saida
+print("Alo mundo!")
+
+# Quando quiser guardar uma String!                             (frase)
+nome = "Pricila Sapper"
+#Quando quiser guardar um número inteiro
+idade = 40
+
+# Exibir o meu nome (que está dentro da variável nome)
+print(nome)
+
+#Quando quiser exibir a frase "Minha idade é " completando com o conteúdo da variável idade
+print("Meu nome é "+nome)
+print("Minha idade é "+str(idade))
+print(f"Minha idade é {idade}\n")
+print("Minha idade é {}".format(idade))
+
+#Quando quiser exibir "Meu nome é...e tenho...anos..." trocando pelas variáveis nome e idade
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
+
+
+
+'''
+Comentario em bloco
+
+'''
+#Estrutura condicional - o famoso "SE" (if)
